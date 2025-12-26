1d0111d158c3c0c243b9e52bac80c08974e09028
Rich Gwilliam <rich.gwilliam@gmail.com>
2025-12-26 22:37:27 +0000

    adding base vue install

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    axios.get('https://api.weather.gov/alerts')
+      .then(response => {
+        console.log('Data fetched:', response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
